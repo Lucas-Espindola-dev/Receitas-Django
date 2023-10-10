819f308201d2097d14f7b20a1aacab3624f19b4b
Lucas Espíndola <lucasespindola.dev@gmail.com>
2023-10-10 15:57:08 -0300

    Update views.py

--- recipes/views.py
+++ recipes/views.py
@@ -25,10 +25,6 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    # recipe = Recipe.objects.filter(
-    #     pk=id,
-    #     is_published=True,
-    # ).order_by('-id').first()
     recipe = get_object_or_404(Recipe, pk=id, is_published=True, )
 
     return render(request, 'recipes/pages/recipe_view.html', context={
