e9a0a133ea40509e5100569c5c43963a00f7e781
Lucas Espíndola <lucasespindola.dev@gmail.com>
2023-10-11 16:09:58 -0300

    Update test_recipe_views.py

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse, resolve
 from recipes import views
-from .test_recipe_base import Recipe, RecipeTestBase
+from .test_recipe_base import RecipeTestBase
 
 
 class RecipeViewsTest(RecipeTestBase):
@@ -111,7 +111,7 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertIn(needed_title, content)
 
     def test_recipe_detail_template_dont_load_recipe_not_published(self):
-        """Test recipe is_published False dont show"""
+        """Test recipe is_published False don't show"""
         # Need a recipe for this test
         recipe = self.make_recipe(is_published=False)
         response = self.client.get(
@@ -122,11 +122,23 @@ class RecipeViewsTest(RecipeTestBase):
                 }))
         self.assertEqual(response.status_code, 404)
 
-
     def test_recipe_search_uses_correct_view_function(self):
         resolved = resolve(reverse('recipes-search'))
         self.assertIs(resolved.func, views.search)
 
     def test_recipe_search_loads_correct_template(self):
-        response = self.client.get(reverse('recipes-search'))
+        response = self.client.get(reverse('recipes-search') + '?q=teste')
         self.assertTemplateUsed(response, 'recipes/pages/search.html')
+
+    def test_recipe_search_raises_404_if_no_search_term(self):
+        url = reverse('recipes-search')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+        
+    def test_recipe_search_term_is_on_page_title_and_escaped(self):
+        url = reverse('recipes-search') + '?q=<Teste>'
+        response = self.client.get(url)
+        self.assertIn(
+            'Search for &quot;&lt;Teste&gt;&quot;',
+            response.content.decode('utf-8')
+        )
