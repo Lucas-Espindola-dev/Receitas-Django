f91282b926efa01f74a151ad689d89c21ec4c4c3
Lucas Espíndola <lucasespindola.dev@gmail.com>
2023-10-12 09:02:17 -0300

    Update views.py

--- recipes/views.py
+++ recipes/views.py
@@ -42,9 +42,10 @@ def search(request):
         raise Http404()
 
     recipes = Recipe.objects.filter(
-        Q(title__icontains=search_term) |
-        Q(description__icontains=search_term),
-    ).order_by('-id')
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),),
+        is_published=True).order_by('-id')
 
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
