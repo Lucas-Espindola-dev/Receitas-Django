6962a79787283e36c219b2749db1a4867ee4533c
Lucas Espíndola <lucasespindola.dev@gmail.com>
2023-10-18 14:59:07 -0300

    Update views.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from recipes.models import Recipe
@@ -8,8 +9,13 @@ def home(request):
     recipes = Recipe.objects.filter(
         is_published=True,
         ).order_by('-id')
+
+    current_page = request.GET.get('page', 1)
+    paginator = Paginator(recipes, 9)
+    page_obj = paginator.get_page(current_page)
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
     })
 
 
