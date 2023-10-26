b0a3c9c7f6a8c22ae963331ae675c78065204517
Lucas Espíndola <lucasespindola.dev@gmail.com>
2023-10-26 14:52:31 -0300

    Update test_recipe_home_view.py

--- recipes/tests/test_recipe_home_view.py
+++ recipes/tests/test_recipe_home_view.py
@@ -1,6 +1,6 @@
 from django.urls import resolve, reverse
 from recipes import views
-
+from unittest.mock import patch
 from .test_recipe_base import RecipeTestBase
 
 
@@ -48,4 +48,18 @@ class RecipeHomeViewTest(RecipeTestBase):
             '<h1>No recipes found here 🥲</h1>',
             response.content.decode('utf-8')
         )
-        
+
+        def test_recipe_home_is_paginated(self):
+            for i in range(8):
+                kwargs = {'slug': f'r{i}', 'author_data': {'username': f'u{i}'}}
+                self.make_recipe(**kwargs)
+
+            with patch('recipes.views.PER_PAGE', new=3):
+                response = self.client.get(reverse('recipes-home'))
+                recipes = response.context['recipes']
+                paginator = recipes.paginator
+
+                self.assertEqual(paginator.num_pages, 3)
+                self.assertEqual(len(paginator.get_page(1)), 3)
+                self.assertEqual(len(paginator.get_page(2)), 3)
+                self.assertEqual(len(paginator.get_page(3)), 2)
