2032c874a3d259f69636afec786411c9dd033b65
Lucas Espíndola <lucasespindola.dev@gmail.com>
2023-10-27 15:49:13 -0300

    Update views.py

--- recipes/views.py
+++ recipes/views.py
@@ -14,9 +14,6 @@ def home(request):
         is_published=True,
         ).order_by('-id')
 
-    messages.error(request, 'Epa, você foi pesquisar algo que eu vi.')
-    messages.success(request, 'Epa, você foi pesquisar algo que eu vi.')
-    messages.info(request, 'Epa, você foi pesquisar algo que eu vi.')
 
     page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
