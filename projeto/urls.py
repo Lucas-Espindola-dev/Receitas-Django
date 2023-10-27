3b7533f6268b3174581408f43da1874eafcce1eb
Lucas Espíndola <lucasespindola.dev@gmail.com>
2023-10-27 15:53:08 -0300

    Update urls.py

--- projeto/urls.py
+++ projeto/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recipes.urls'))
+    path('', include('recipes.urls')),
+    path('authors/', include('authors.urls')),
 ]
